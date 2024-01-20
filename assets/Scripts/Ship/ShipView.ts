68ab5050a9e55d661eaf789c1731c2d2c47430a6
Zarrickisdea <mohammedraz45@yahoo.com>
2024-01-20 12:32:54 +0530

    First commit

---
+++ assets/Scripts/Ship/ShipView.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ShipView')
+export class ShipView extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/Scripts/State Machine/BaseState.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BaseState')
+export class BaseState extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
